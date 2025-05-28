441b73d32c804226270511f3e9876460ee03ebac
aashkadesai <ceaashkadesai@gmail.com>
2025-05-28 17:38:58 -0400

    Time: 3 ms (88.19%), Space: 18.9 MB (82.53%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -7,37 +7,15 @@ class Solution:
             graph[src].append(dest)
             indegree[dest] += 1
         
-        # q = deque([i for i in range(numCourses) if indegree[i] == 0])
-        # visited = 0
+        q = deque([i for i in range(numCourses) if indegree[i] == 0])
+        visited = 0
 
-        # while q:
-        #     course = q.popleft()
-        #     visited += 1
-        #     for nbr in graph[course]:
-        #         indegree[nbr] -= 1
-        #         if indegree[nbr] == 0:
-        #             q.append(nbr)
-        # return visited == numCourses
-
-        visited = [0] * numCourses
-
-        def dfs(course):
-            if visited[course] == 1:
-                return False
-            if visited[course] == 2:
-                return True
-                
-            visited[course] = 1
-
+        while q:
+            course = q.popleft()
+            visited += 1
             for nbr in graph[course]:
-                if not dfs(nbr):
-                    return False
-            
-            visited[course] = 2
-            return True
-        
-        for i in range(numCourses):
-            if not dfs(i):
-                return False
-        return True
+                indegree[nbr] -= 1
+                if indegree[nbr] == 0:
+                    q.append(nbr)
+        return visited == numCourses
             
