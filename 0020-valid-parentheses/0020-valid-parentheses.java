a5fed6fb414f50015254b7f17ee6075daf4aaca6
aashkadesai <ceaashkadesai@gmail.com>
2025-06-01 11:38:35 -0400

    Time: 3 ms (41.08%), Space: 41.7 MB (66.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,13 +1,33 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack = new Stack<>();
+
+//         for(char ch: s.toCharArray()){
+//             if (ch == '(') stack.push(')');
+//             else if (ch == '[') stack.push(']');
+//             else if (ch == '{') stack.push('}');
+//             else{
+//                 if(stack.isEmpty() || stack.pop() != ch) return false;
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
+
+
 class Solution {
     public boolean isValid(String s) {
+        Map<Character, Character> braces = new HashMap<>();
+        braces.put('(', ')');
+        braces.put('[', ']');
+        braces.put('{', '}');
+
         Stack<Character> stack = new Stack<>();
 
         for(char ch: s.toCharArray()){
-            if (ch == '(') stack.push(')');
-            else if (ch == '[') stack.push(']');
-            else if (ch == '{') stack.push('}');
-            else{
-                if(stack.isEmpty() || stack.pop() != ch) return false;
+            if (braces.containsKey(ch)) stack.push(braces.get(ch));
+            else {
+                if (stack.isEmpty() || stack.pop() != ch) return false;
             }
         }
         return stack.isEmpty();
