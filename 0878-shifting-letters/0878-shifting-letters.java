9641f8192608f6077ea7e1158e7bec31253ed4a8
aashkadesai <ceaashkadesai@gmail.com>
2025-08-13 23:18:12 -0400

    Time: 6 ms (90.68%), Space: 57.2 MB (61.59%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.java
+++ 0878-shifting-letters/0878-shifting-letters.java
@@ -1,27 +1,34 @@
+// class Solution {
+//     public String shiftingLetters(String s, int[] shifts) {
+//         char[] sList = s.toCharArray();
+//         int n = sList.length;
+
+//         for (int i = n - 2; i >= 0; i--) {
+//             shifts[i] = (shifts[i] + shifts[i + 1]) % 26;
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             char c = sList[i];
+//             char shiftedChar = (char)((c - 'a' + (int)shifts[i]) % 26 + 'a');
+//             sList[i] = shiftedChar;
+//         }
+
+//         return new String(sList);
+//     }
+// }
+
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
-        char[] sList = s.toCharArray();
-        int n = sList.length;
-        long[] longShifts = new long[n];
+        int n = s.length();
+        long totalShifts = 0;
+        char[] res = new char[n];
 
-        // Copy shifts into long[]
-        for (int i = 0; i < n; i++) {
-            longShifts[i] = shifts[i];
+        for (int i = n - 1; i >= 0; i--) {
+            totalShifts = (totalShifts + shifts[i]) % 26;
+            char original = s.charAt(i);
+            int shifted = (original - 'a' + (int)totalShifts) % 26;
+            res[i] = (char)('a' + shifted);
         }
-
-        // Accumulate shifts from right to left (mod 26 early to avoid overflow)
-        for (int i = n - 2; i >= 0; i--) {
-            longShifts[i] = (longShifts[i] + longShifts[i + 1]) % 26;
-        }
-
-        // Apply shifts safely
-        for (int i = 0; i < n; i++) {
-            int shift = (int)(longShifts[i] % 26);
-            char c = sList[i];
-            char shiftedChar = (char)((c - 'a' + shift) % 26 + 'a');
-            sList[i] = shiftedChar;
-        }
-
-        return new String(sList);
+        return new String(res);
     }
 }
