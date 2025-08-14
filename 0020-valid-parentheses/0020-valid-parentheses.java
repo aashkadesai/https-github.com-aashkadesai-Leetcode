4329c682b583a5419e4f839d067a7aea40e82dfc
aashkadesai <ceaashkadesai@gmail.com>
2025-08-13 21:41:52 -0400

    Time: 3 ms (37.69%), Space: 42.4 MB (6.14%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -15,21 +15,49 @@
 // }
 
 
+// class Solution {
+//     public boolean isValid(String s) {
+//         Map<Character, Character> braces = new HashMap<>();
+//         braces.put('(', ')');
+//         braces.put('[', ']');
+//         braces.put('{', '}');
+
+//         Stack<Character> stack = new Stack<>();
+
+//         for(char ch: s.toCharArray()){
+//             if (braces.containsKey(ch)) stack.push(braces.get(ch));
+//             else {
+//                 if (stack.isEmpty() || stack.pop() != ch) return false;
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
+
 class Solution {
+
     public boolean isValid(String s) {
         Map<Character, Character> braces = new HashMap<>();
-        braces.put('(', ')');
-        braces.put('[', ']');
-        braces.put('{', '}');
+        
+        braces.put(')', '(');
+        braces.put(']', '[');
+        braces.put('}', '{');
 
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stk = new Stack<>();
 
-        for(char ch: s.toCharArray()){
-            if (braces.containsKey(ch)) stack.push(braces.get(ch));
+        for(char ch: s.toCharArray()) {
+            if (braces.containsValue(ch)) {
+                stk.push(ch);
+            }
+            else if (braces.containsKey(ch)) {
+                if (stk.isEmpty() || stk.pop() != braces.get(ch)){
+                    return false;
+                }
+            }
             else {
-                if (stack.isEmpty() || stack.pop() != ch) return false;
+                return false;
             }
         }
-        return stack.isEmpty();
+        return stk.isEmpty();
     }
 }
