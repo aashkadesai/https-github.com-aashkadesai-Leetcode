2fe120c1c110e9d700d0865f70e0a74dbc01562c
aashkadesai <ceaashkadesai@gmail.com>
2025-08-13 22:07:11 -0400

    Time: 5 ms (89.34%), Space: 44.7 MB (70.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,22 +1,41 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int i = 0, j = 0;
+//         int maxLength = 0;
+//         List<Character> sList = new ArrayList<>();
+
+//         while (j < s.length()) {
+//             char curr = s.charAt(j);
+
+//             if (sList.contains(curr)) {
+//                 sList.remove((Character) s.charAt(i));  
+//                 i++;
+//             } else {
+//                 sList.add(curr);
+//                 maxLength = Math.max(maxLength, j - i + 1);
+//                 j++;
+//             }
+//         }
+
+//         return maxLength;
+//     }
+// }
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int i = 0, j = 0;
+        Map<Character, Integer> lastSeen = new HashMap<>();
+        int start = 0;
         int maxLength = 0;
-        List<Character> sList = new ArrayList<>();
 
-        while (j < s.length()) {
-            char curr = s.charAt(j);
+        for(int end = 0; end < s.length(); end++) {
+            char current = s.charAt(end);
 
-            if (sList.contains(curr)) {
-                sList.remove((Character) s.charAt(i));  
-                i++;
-            } else {
-                sList.add(curr);
-                maxLength = Math.max(maxLength, j - i + 1);
-                j++;
+            if(lastSeen.containsKey(current) && lastSeen.get(current) >= start) {
+                start = lastSeen.get(current) + 1;
             }
+        lastSeen.put(current, end);
+        maxLength = Math.max(maxLength, end - start + 1);
         }
-
         return maxLength;
     }
 }
