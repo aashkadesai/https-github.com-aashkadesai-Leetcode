abbeb016006f10437cb100e41e1957cbd796fe5b
aashkadesai <ceaashkadesai@gmail.com>
2025-08-14 21:30:52 -0400

    Time: 0 ms (100%), Space: 41.7 MB (66.28%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,18 +13,37 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> resu = new ArrayList<>();
+//         postorder(root, result);
+//         return result;
+//     }
+
+//     private void postorder(TreeNode node, List<Integer> result) {
+//         if(node == null) return;
+
+//         postorder(node.left, result);
+//         postorder(node.right, result);
+//         result.add(node.val);
+//     }
+// }
+
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        postorder(root, result);
+        if (root == null) return result;
+
+        Stack<TreeNode> stk = new Stack<>();
+        stk.push(root);
+
+        while(!stk.isEmpty()) {
+            TreeNode node = stk.pop();
+            result.add(node.val);
+            if(node.left != null) stk.push(node.left);
+            if(node.right != null) stk.push(node.right);
+        }
+        Collections.reverse(result);
         return result;
-    }
-
-    private void postorder(TreeNode node, List<Integer> result) {
-        if(node == null) return;
-
-        postorder(node.left, result);
-        postorder(node.right, result);
-        result.add(node.val);
     }
 }
